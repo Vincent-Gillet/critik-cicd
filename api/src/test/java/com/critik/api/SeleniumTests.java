ec9252f1e5805537286dd2b58b0b387d89339e28
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2025-09-17 18:55:49 +0200

    test pipeline cicd

--- api/src/test/java/com/critik/api/SeleniumTests.java
+++ api/src/test/java/com/critik/api/SeleniumTests.java
@@ -1,7 +1,14 @@
 package com.critik.api;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.Wait;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 @SpringBootTest
 class SeleniumTests {
@@ -29,8 +36,8 @@ class SeleniumTests {
         driver.findElement(By.id("passwordUserValidationSubscription")).sendKeys("john1234");
         driver.findElement(By.xpath("/html/body/app-root/app-subcribe/section/div/form/div[6]/button")).click();
 
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(
+        Wait<WebDriver> waitLogin = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitLogin.until(
                 ExpectedConditions.elementToBeClickable(By.id("emailLogin"))
         );
 
@@ -38,8 +45,8 @@ class SeleniumTests {
         driver.findElement(By.id("passwordLogin")).sendKeys("john1234");
         driver.findElement(By.xpath("/html/body/app-root/app-login/form/div[3]/button")).click();
 
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(
+        Wait<WebDriver> waitNavbar = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitNavbar.until(
                 ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"navbarNav\"]/ul/li[1]/a"))
         );
 
