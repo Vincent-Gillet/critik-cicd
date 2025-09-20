f1893553d9fae5f0fdb2593eceef1c31a613fa3f
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2025-09-20 12:15:39 +0200

    test pipeline cicd

--- api/src/main/java/com/critik/api/config/SecurityConfig.java
+++ api/src/main/java/com/critik/api/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(
